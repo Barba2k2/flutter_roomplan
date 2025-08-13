17a100cc8a76b87484fdadd81ff3e8c4c422157f
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-13 19:32:56 -0300

    feat: update main app example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:example/views/home_page.dart'; // Import HomePage
-import 'package:provider/provider.dart'; // Import provider
-import 'package:example/viewmodels/scanner_view_model.dart'; // Import ScannerViewModel
-import 'package:example/viewmodels/home_view_model.dart'; // Import HomeViewModel
+import 'package:example/views/home_page.dart';
+import 'advanced_scanning_page.dart';
+import 'package:provider/provider.dart';
+import 'package:example/viewmodels/scanner_view_model.dart';
+import 'package:example/viewmodels/home_view_model.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,12 +21,53 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MultiProvider( // Use MultiProvider for multiple ViewModels
+      home: MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => HomeViewModel()),
           ChangeNotifierProvider(create: (context) => ScannerViewModel()),
         ],
-        child: const HomePage(), // Set HomePage as the home
+        child: const MainNavigationPage(),
+      ),
+    );
+  }
+}
+
+class MainNavigationPage extends StatefulWidget {
+  const MainNavigationPage({super.key});
+
+  @override
+  State<MainNavigationPage> createState() => _MainNavigationPageState();
+}
+
+class _MainNavigationPageState extends State<MainNavigationPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const AdvancedScanningPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Simple',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_input_antenna),
+            label: 'Advanced',
+          ),
+        ],
       ),
     );
   }
